313f0f85fe81a229b43bf1560032e7242ab14353
Huri2o <udelrealalvarez@gmail.com>
2025-05-19 17:55:13 -0600

    funcional

---
+++ global.php
@@ -0,0 +1,16 @@
+<?php
+//codificacion de caracteres
+define("DB_ENCODE","utf8");
+//NOMBRE DEL PROYECTO
+define("PRO_NOMBRE","CLIENTE SERVIDOR");
+//HOST
+define("DB_HOST","localhost");
+//nombre de la base de datos
+define("DB_NAME","practica");
+//usuario de la base de datos
+define("DB_USER","root");
+//contraseña de la base de datos
+define("DB_PASSWORD","");
+
+
+?>
