fbb22f9b572b8e07d338d972f53695df03e3f067
Huri2o <udelrealalvarez@gmail.com>
2025-06-10 07:11:57 -0600

    con Productos

---
+++ vistas/js/Productos.js
@@ -0,0 +1,28 @@
+let ProductisTidis = [];
+let productosFiltrados = [];
+let paginaActual = 1;
+const POR_PAGINA = 9;
+function cargarProductos() {
+    fetch('../ajax/Productos.php')
+        .then(response => response.json())
+        .then(productos => {
+            productosTodos = productos;
+            productosFiltrados = productos;
+            renderProductos ();
+            agregarListeners();
+        })
+        .catch(error => console.error('Error al cargar los productos:', error));
+}
+
+function agregarListeners() {
+    const serchInput = document.querySelectorAll('serch-input');
+    if (serchInput){
+        document.querySelector('[id^="price-"]').forEach(cb => {
+            cb.addEventListener('change', aplicarFiltros);
+        })
+    }
+}
+function aplicarFiltros() {
+    const search = document.getElementById('search-input')?.toLowerCase() || '';
+    const precios = Array.from(document.querySelectorAll('[id^="price-"]:checked')).map(c => c.id);
+}
